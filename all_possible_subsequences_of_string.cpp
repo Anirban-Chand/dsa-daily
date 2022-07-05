04e015a8ea78c6ed37914457efe736cf6ddd2194
Anirban-Chand <anirbanchand54@gmail.com>
2022-07-06 00:54:42 +0530

    Recursive Solution

---
+++ all_possible_subsequences_of_string.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void findAllSubsequences(string s, int idx, string subsequence, vector<string>& ans){
+    // base case
+    if(idx >= s.size()){
+        ans.push_back(subsequence);
+        return;
+    }
+
+    // exclude
+    findAllSubsequences(s, idx+1, subsequence, ans);
+
+    // include
+    int ch = s[idx];
+    subsequence+=ch;
+    findAllSubsequences(s, idx+1, subsequence, ans);
+}
+
+int main(){
+    string s = "abcde";
+    vector<string> ans;
+    int idx = 0;
+    string subsequence = "";
+    findAllSubsequences(s, idx, subsequence, ans);
+    for(int i=0; i<ans.size(); i++)
+        cout<<"subsequence #"<<i+1<<":  "<<ans[i]<<endl;
+    
+
+    return 0;
+}
