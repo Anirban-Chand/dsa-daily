fb3636fc9d893a6a6515554a13b793ffad9af131
Anirban-Chand <anirbanchand54@gmail.com>
2022-07-13 23:47:30 +0530

    Get Maximum in Generated Array - Leetcode 1646

--- Get_Maximum_in_Generated_Array.cpp
+++ Get_Maximum_in_Generated_Array.cpp
@@ -12,3 +12,20 @@ public:
         return *max_element(v.begin(), v.end());
     }
 };
+
+
+
+// Some bit shifting actually worked pretty well
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if(n==0) return 0;
+        vector<int> v(n+1, 0);
+        v[1]=1;
+        for(int i=1; 2*i+1<=n; i++){
+            v[i<<1] = v[i];
+            v[(i<<1) +1] = v[i]+v[i+1];
+        }
+        return *max_element(v.begin(), v.end());
+    }
+};
