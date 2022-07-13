c0d211b4bdce5e7054c20c96434e4e88eae63465
Anirban-Chand <anirbanchand54@gmail.com>
2022-07-13 12:43:13 +0530

    Is Subsequence - Leetcode 392

---
+++ IsSubsequence.cpp
@@ -0,0 +1,22 @@
+// BRUTE FORCE RECURSIVE SOLUTION
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {        
+        string temp="";
+        return solve(s,t,0,temp);
+    }
+    
+    bool solve(string &s, string &t, int i, string temp){
+        if(i==t.size()){
+            if(temp == s) 
+                return 1;
+            return 0;
+        }
+        
+        bool dontTake = solve(s, t, i+1, temp);
+        
+        temp+=t[i];
+        bool take = solve(s,t,i+1,temp);
+        return take or dontTake;
+    }
+};
